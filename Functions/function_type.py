8221724d288ccf810a4b340d355aca4a02ccad3f
andreferreiraext123 <mf1903491@gmail.com>
2024-11-02 10:10:28 -0300

    Adding new function explanation 'function type'

---
+++ Functions/function_type.py
@@ -0,0 +1,12 @@
+# A funcao type retorna o tipo do dado
+
+# Storing data
+name = 'Andre'
+age = 18
+weight = 60.2
+
+# Printing data type
+
+print(f' {name}, {type(name)}')
+print(f' {age}, {type(age)}')
+print(f' {weight}, {type(weight)}')
